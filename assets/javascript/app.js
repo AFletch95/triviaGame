f17d9fbb231b7cc0bf77b66f301f5ac675afe562
Afletch <austinj.fletcher@gmail.com>
2019-08-17 18:25:59 -0700

    created play button, quiz timer and show answerfunctions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,12 +6,14 @@ var $answer1Text = $('#answer1Text');
 var $answer2Text = $('#answer2Text');
 var $answer3Text = $('#answer3Text');
 var $answer4Text = $('#answer4Text');
-var $playButtonDiv = $('#playButton');
+var $playButtonDiv = $('#playButtonDiv');
+
+
 
 var questionIndex = 0;
-
-
-var triviaArray = [question0,question1] //Array that contains all the questions.
+var qSeconds = 5; //TODO: set to 8 when finished
+var Aseconds = 5;
+var correct = true;
 
 class trivaQuestions {
   constructor(index,question,answer1,answer2,answer3,correctAnswer){
@@ -25,24 +27,111 @@ class trivaQuestions {
 }
 
 var question0 = new trivaQuestions(0,"The beaver is the national emblem of which country?",
-                                   "Iceland","Switzerland","Austria","Canada");
+"Iceland","Switzerland","Austria","Canada");
 var question1 = new trivaQuestions(0,"What is the largest Ocean?",
-                                   "Alantic Ocean","Artic Ocean","Indian Ocean","Pacific Ocean");
+"Alantic Ocean","Artic Ocean","Indian Ocean","Pacific Ocean");
+
+var triviaArray = [question0,question1] //Array that contains all the questions.
+var currentQuestionClass = triviaArray[questionIndex]; //holds the current question class
+
+createPlayButton();
 
 function randNum1_4(){
   return Math.floor(Math.random()*4)+1;
 }
+function randAnswerPlacement(obj){
+  let rand = randNum1_4();
+  console.log(rand);
 
-function createPlayButton(){
-  $playButtonDiv.append("<button id='playButton'>'Take the Test!</button>");
-  $('#playButton').html("class","btn-lg btn-primary");
+  //Depending on what number rand is put the correct answer in that box
 
-  $('#playButton').on("click",function(){
-
-    $currentQuestion = triviaArray[questionIndex];
-    trivaQuestions++;
-
-    
-    // put timer function here
-  });
+  if(rand == 1){
+    $answer1Text.text(obj.correctAnswer);
+    $answer2Text.text(obj.answer3);
+    $answer3Text.text(obj.answer2);
+    $answer4Text.text(obj.answer1);
+    return rand;
+  }
+  
+  if(rand == 2){
+    $answer1Text.text(obj.answer3);
+    $answer2Text.text(obj.correctAnswer);
+    $answer3Text.text(obj.answer1);
+    $answer4Text.text(obj.answer2);
+    return rand;
+  }
+  
+  if(rand == 3){
+    $answer1Text.text(obj.answer1);
+    $answer2Text.text(obj.answer2);
+    $answer3Text.text(obj.correctAnswer);
+    $answer4Text.text(obj.answer3);
+    return rand;
+  }
+  
+  if(rand == 4){
+    $answer1Text.text(obj.answer3);
+    $answer2Text.text(obj.answer1);
+    $answer3Text.text(obj.answer2);
+    $answer4Text.text(obj.correctAnswer);
+    return rand;
+  }
+  else return false;
 }
+//DONE
+function startTimer(){
+  clearInterval(quizTimer);
+  seconds = 7;
+  var quizTimer = setInterval(showClock,1000);
+  function showClock(){
+    $timer.text(qSeconds+" Seconds remaining!")
+    qSeconds--;
+    if(qSeconds < 0){
+      $timer.empty();
+      clearInterval(quizTimer);
+      showAnswer();
+    }
+  }
+}
+function showAnswer(){
+  if(correct){
+    $currentQuestion.text("Correct!");
+    $answer1Text.text("The answer was:");
+  }
+  else {
+    $currentQuestion.text("Wrong!");
+    $answer1Text.text("The correct answer was:");
+  }
+  
+  //display correct answer.
+  $answer2Text.text(currentQuestionClass.correctAnswer);
+  $answer3Text.empty();
+  let queryUrl = "https://api.giphy.com/v1/gifs/random?tag="+currentQuestionClass.correctAnswer+"&api_key=Jndh9UXi9mJk2q9APuSJ2tTC9jryrwhm"
+  $.ajax({url: queryUrl,method:"GET"})
+  .then(function(response){
+    var img = $('<img>');
+    img.attr("src",response.data.url);
+    $answer3Text.append(img);
+    console.log(response);
+
+  })
+  $answer4Text.empty();
+}
+//DONE
+function createPlayButton(){
+  console.log("hi")
+  $playButtonDiv.append("<button>");
+  $('button').text("Take the test!")
+  $('button').attr("id","playButton");
+  $('#playButton').attr("class","btn-lg btn-primary");
+}
+//DONE
+$('#playButton').on("click",function(){ 
+  $(this).remove();
+  currentQuestionClass = triviaArray[questionIndex];  
+  $currentQuestion.text(currentQuestionClass.question);
+  randAnswerPlacement(currentQuestionClass);
+  $questionNumber.text(questionIndex+1+" / 2");
+  startTimer();
+  // put timer function here
+});
